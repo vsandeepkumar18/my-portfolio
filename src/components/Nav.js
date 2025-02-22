ff8a514a4c4fc70553b84107d151ffa110909711
vsandeepkumar18 <sandeepkumarv380@gmail.com>
2025-02-22 13:36:26 +0530

    nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React from "react";
+// import the icons
+
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+
+import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+
+// link
+
+import { Link } from "react-scroll";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
+      <div className="container mx-auto">
+        {/* nav inner */}
+        <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
+          <a href="#" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+            <BiHomeAlt />
+          </a>
+          <a href="#"className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+            <BiUser/>
+          </a>
+          <a href="#" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+            <BsClipboardData />
+          </a>
+          <a href="#" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+            <BsBriefcase />
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
