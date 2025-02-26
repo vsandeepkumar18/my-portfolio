b640ac36f06c07b4fa38ce7be8dfee7a83b725c0
vsandeepkumar18 <sandeepkumarv380@gmail.com>
2025-02-26 20:13:28 +0530

    banner and about updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,71 @@
-import React from 'react';
-
+import React from "react";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 const About = () => {
-  return <div className='section' id='about'>About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return (
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div>
+          {/* img */}
+          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
+          {/* text */}
+          <div>
+            <h2>About Me</h2>
+            <h3>I am Sandeep Kumar, a passionate web developer with expertise in **React.js, Next.js, and Tailwind CSS**.
+            Currently pursuing **MCA**, I build highly interactive, modern web applications with a focus on
+            performance and user experience.</h3>
+            {/* stats */}
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {
+                    inView?
+                    <CountUp start={0} end={7} duration={3} /> : null
+                    }
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of <br />Experience
+                </div>
+              </div>
+
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {
+                    inView?
+                    <CountUp start={0} end={3} duration={3} /> : null
+                    } k+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Projects <br />Completed
+                </div>
+              </div>
+
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {
+                    inView?
+                    <CountUp start={0} end={7} duration={3} /> : null
+                    }
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of <br />Experience
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  );
 };
 
 export default About;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -15,11 +15,11 @@ const Banner = () => {
 
           <div className="flex-1 text-center font-secondary lg:text-left">
 
-            <motion.h1 variants={fadeIn('up',0.5)} initial="hidden" whileInView={'show'} viewport={{once:false, amount:0.7}} className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
+            <motion.h1 variants={fadeIn('up',0.3)} initial="hidden" whileInView={'show'} viewport={{once:false, amount:0.7}} className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
               SANDEEP <span>V</span>
             </motion.h1>
 
-            <motion.div variants={fadeIn('up',0.5)} initial="hidden" whileInView={'show'} viewport={{once:false, amount:0.7}} className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+            <motion.div variants={fadeIn('up',0.3)} initial="hidden" whileInView={'show'} viewport={{once:false, amount:0.7}} className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
               <span className="mr-4 text-white">Iam a </span>
               <TypeAnimation
                 sequence={[
@@ -38,7 +38,7 @@ const Banner = () => {
               />
             </motion.div>
 
-            <motion.p variants={fadeIn('up',0.7)} initial="hidden" whileInView={'show'} viewport={{once:false, amount:0.7}} className="mb-8 max-w-lg mx-auto lg:mx-0">
+            <motion.p variants={fadeIn('up',0.5)} initial="hidden" whileInView={'show'} viewport={{once:false, amount:0.7}} className="mb-8 max-w-lg mx-auto lg:mx-0">
               Frontend Developer specializing in React. js and
               Passionate about crafting visually stunning,
               highly interactive, and user-centric web applications. Currently
@@ -46,14 +46,14 @@ const Banner = () => {
               projects.
             </motion.p>
 
-            <motion.div variants={fadeIn('up',0.9)} initial="hidden" whileInView={'show'} viewport={{once:false, amount:0.7}} className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+            <motion.div variants={fadeIn('up',0.5)} initial="hidden" whileInView={'show'} viewport={{once:false, amount:0.7}} className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
               <button className="btn btn-lg">Contact Me</button>
               <a href="" className="text-gradient btn-link">My Portfolio</a>
             </motion.div>
 
             {/* social media*/}
 
-            <motion.div variants={fadeIn('up',0.9)} initial="hidden" whileInView={'show'} viewport={{once:false, amount:0.7}} className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+            <motion.div variants={fadeIn('up',0.7)} initial="hidden" whileInView={'show'} viewport={{once:false, amount:0.7}} className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
               <a href=""><FaYoutube/></a>
               <a href=""><FaGithub/></a>
               <a href=""><FaDribbble/></a>
@@ -63,7 +63,7 @@ const Banner = () => {
 
           {/* image */}
 
-          <motion.div variants={fadeIn('down',0.9)} initial="hidden" whileInView={'show'} className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
+          <motion.div variants={fadeIn('down',0.7)} initial="hidden" whileInView={'show'} className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
             <img src={Image} alt="" />
           </motion.div>
         
