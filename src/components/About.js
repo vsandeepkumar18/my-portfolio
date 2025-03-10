1fea4544c526aaf027bcaa86c4c9a23d42714e4c
vsandeepkumar18 <sandeepkumarv380@gmail.com>
2025-03-10 08:07:00 +0530

    about section completed

--- src/components/About.js
+++ src/components/About.js
@@ -8,62 +8,79 @@ const About = () => {
     threshold: 0.5,
   });
   return (
-    <section className="section" id="about" ref={ref}>
+    <section className="section mt-10" id="about" ref={ref}>
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           {/* img */}
-          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
+
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top">
+          </motion.div>
+
           {/* text */}
-          <div>
-            <h2>About Me</h2>
-            <h3>I am Sandeep Kumar, a passionate web developer with expertise in **React.js, Next.js, and Tailwind CSS**.
-            Currently pursuing **MCA**, I build highly interactive, modern web applications with a focus on
-            performance and user experience.</h3>
+        <motion.div
+          variants={fadeIn("left", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }} className="flex-1">
+            <h2 className="text-accent h2">About Me</h2>
+            <h3 className="h3 mb-4">
+              I am Sandeep Kumar, a passionate web developer with expertise in
+              React.js, Next.js, and Tailwind CSS.
+            </h3>
+            <p className="mb-6">
+              Currently pursuing MCA, I build highly interactive, modern web
+              applications with a focus on performance and user experience.
+            </p>
             {/* stats */}
             <div className="flex gap-x-6 lg:gap-x-10 mb-12">
-
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {
-                    inView?
-                    <CountUp start={0} end={7} duration={3} /> : null
-                    }
+                  {inView ? <CountUp start={0} end={7} duration={3} /> : null}
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
-                  Years of <br />Experience
+                  Years of <br />
+                  Experience
                 </div>
               </div>
 
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {
-                    inView?
-                    <CountUp start={0} end={3} duration={3} /> : null
-                    } k+
+                  {inView ? <CountUp start={0} end={60} duration={3} /> : null}+
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
-                  Projects <br />Completed
+                  Projects <br />
+                  Completed
                 </div>
               </div>
 
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {
-                    inView?
-                    <CountUp start={0} end={7} duration={3} /> : null
-                    }
+                  {inView ? <CountUp start={0} end={100} duration={3} /> : null}
+                  +
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
-                  Years of <br />Experience
+                  Happy <br />
+                  Clients
                 </div>
               </div>
-
-
             </div>
-          </div>
+
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact Me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+
+          </motion.div>
+          
         </div>
       </div>
-
     </section>
   );
 };
